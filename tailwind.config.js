0ec0c7c7afa30a1be164cc340feb67b2fb5e87fc
sfvidrerias-design <sfvidrerias@gmail.com>
2025-11-20 09:12:47 -0600

    Create tailwind.config.js
    Commit

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: { extend: { colors: { primary: '#0f4c3a', accent: '#28a745' } } },
+  plugins: [],
+};
